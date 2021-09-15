24885697cbf4be0df763a8c348af454b6b9668a1
Denis <example@gmail.com>
2021-09-15 12:51:58 +0300

    upg cycles and add string verification

--- src/Js/script.js
+++ src/Js/script.js
@@ -10,13 +10,31 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов ?", ''),
-      b = +prompt("На сколько его оцените ?", ''),
-      c = prompt("Один из последних просмотренных фильмов ?", ''),
-      d = prompt("На сколько его оцените ?", '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов ?", ''),
+          b = prompt("На сколько его оцените ?", '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
 
